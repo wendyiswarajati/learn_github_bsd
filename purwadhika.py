bb8a1258f3a50c15d9023312912cfe7bdbd7b146
wendy <wendy.iswarajati@gmail.com>
2019-04-30 20:25:10 +0700

    add purwadhika.py

---
+++ purwadhika.py
@@ -0,0 +1,24 @@
+print('Hello World!!!');
+print('satu dua tiga');
+print('tes tes tes');
+
+nama = 'Wendy';print(nama);
+usia = 35;
+usia = 22;
+jomblo = False;
+contoh = [1,2,3];
+print(usia*4); 
+print('test');
+print(jomblo);
+
+print(type(nama));
+print(type(usia));
+print(type(jomblo));
+print(type(contoh));
+
+
+namaku = input ("Siapa namamu? :");
+print(namaku);
+
+
+
