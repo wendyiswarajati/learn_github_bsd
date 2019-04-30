ba1a12e1a83c29b0dd2295066710f0d63762550d
wendy <wendy.iswarajati@gmail.com>
2019-04-30 20:40:06 +0700

    add strings.py

---
+++ strings.py
@@ -0,0 +1,42 @@
+# Strings
+# x = 'Halo Dunia';
+# print (len(x));
+# print (x.index(' Dunia'));
+# print (x.split(' '));
+# print (x.lower());
+# print (x.upper());
+# print (x.capitalize());
+# # print (x);
+# singleQuotes = 'single quote';
+# doubleQuotes = "double quotes";
+# combineQuotes = "wrap lot's of other quotes";
+
+# print (singleQuotes);
+# print (doubleQuotes);
+# print (combineQuotes);
+
+# String Indexing
+# text = "I'm Wendy, nice to meet you";
+# print (text[1]);
+# print (text[2:]);
+# print (text[:3]);
+# print (text[2:5]);
+# print (text[:]);
+
+# String to Number
+# angka1 = input("Masukkan Angka 1 : " );
+# angka2 = input("Masukkan Angka 2 : " );
+# print (angka1 + angka2);
+# print (int(angka1) + int(angka2));
+# angka1 = float (angka1);
+# angka2 = float (angka2);
+# print (angka1 + angka2);
+
+# Adding String and Number
+usia = 22;
+nama = "Andi";
+
+print (usia + usia);
+print (nama + ' ' + nama);
+
+
